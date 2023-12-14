d8f61cb3cfad80d7fb8bd1e0c22bb44fbe30bb07
binayrijal <binayrijal22@gmail.com>
2023-12-14 17:52:31 +0545

    we extract dataframe from model to this and show in option

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import pandas as pd
+import  pickle
 
+movies_list=pickle.load(open('moviedict.pkl','rb'))
+movies= pd.DataFrame(movies_list)
 st.title('Movie Recomendations system')
 
 option = st.selectbox(
     'How would you like to be contacted?',
-    ('Email', 'Home phone'))
+    movies['title'].values)
 
